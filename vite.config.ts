e8563593bb7ba12c4c060c385e987a5a1e15e731
jawadbhm <66577435+jawadbhm@users.noreply.github.com>
2025-11-26 23:17:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/', // Ensure this matches your deployment path
       server: {
         port: 3000,
         host: '0.0.0.0',
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: true, // Helps with debugging
       },
       plugins: [react()],
       define: {
